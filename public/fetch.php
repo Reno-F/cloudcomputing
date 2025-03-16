cda2c3c00dcd91f56cd859c7ed5f3af7f43cf235
RENO-LAPTOP\ASUS <renovansetio02@gmail.com>
2025-03-17 00:10:00 +0700

    Perubahan

--- public/fetch.php
+++ public/fetch.php
@@ -5,16 +5,23 @@ $ref = $database->getReference('users')->getValue();
 
 if ($ref) {
     foreach ($ref as $key => $value) {
+        $name = isset($value['name']) ? $value['name'] : 'N/A';
+        $email = isset($value['email']) ? $value['email'] : 'N/A';
+        $gender = isset($value['gender']) ? $value['gender'] : 'N/A';
+        $phone = isset($value['phone']) ? $value['phone'] : 'N/A';
+
         echo "<tr>
-                <td>{$value['name']}</td>
-                <td>{$value['email']}</td>
-                <td>{$value['gender']}</td>
-                <td>{$value['phone']}</td>
+                <td>{$name}</td>
+                <td>{$email}</td>
+                <td>{$gender}</td>
+                <td>{$phone}</td>
                 <td>
-                    <button class='btn btn-warning btn-sm' onclick=\"openUpdateModal('$key', '{$value['name']}', '{$value['email']}')\">Edit</button>
+                    <button class='btn btn-warning btn-sm' onclick=\"openUpdateModal('$key', '{$name}', '{$email}')\">Edit</button>
                     <a href='delete.php?id=$key' class='btn btn-danger btn-sm'>Hapus</a>
                 </td>
               </tr>";
     }
+} else {
+    echo "<tr><td colspan='5' class='text-center'>Tidak ada data pengguna.</td></tr>";
 }
 ?>
