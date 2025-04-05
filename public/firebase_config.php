0cfda63ea6e90bb3fa65be4f9f1a34c1ba2fb1d5
RENO-LAPTOP\ASUS <renovansetio02@gmail.com>
2025-04-05 23:26:14 +0700

    Perbaikan

--- public/firebase_config.php
+++ public/firebase_config.php
@@ -1,19 +1,20 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Kreait\Firebase\Factory;
 
-$firebaseCredentials = getenv('FIREBASE_CREDENTIALS');
+$firebaseCredentialsPath = __DIR__ . '/../firebase_credentials.json';
 
-if (!$firebaseCredentials) {
-    die("Firebase credentials not set in environment variables.");
+if (!file_exists($firebaseCredentialsPath)) {
+    die("Firebase credentials file not found.");
 }
 
-$serviceAccount = json_decode($firebaseCredentials, true);
+$serviceAccount = json_decode(file_get_contents($firebaseCredentialsPath), true);
 
 if (!$serviceAccount) {
     die("Invalid Firebase credentials.");
 }
+
 
 $factory = (new Factory)
     ->withServiceAccount($serviceAccount)
@@ -21,4 +22,3 @@ $factory = (new Factory)
 
 $database = $factory->createDatabase();
 $auth = $factory->createAuth();
-?>
